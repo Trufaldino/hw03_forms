97afb3eb3f4216aaaed5fff714766ae70ebf9348
Антон Рядинский <anton.ryadinski@gmail.com>
2022-04-28 18:10:31 +0300

    Пройдены все тесты

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,3 @@
-from re import template
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
@@ -10,26 +9,32 @@ SYMBOLS_PER_PAGE = 10
 
 
 def index(request):
-    post_list = Post.objects.all().order_by('-pub_date')
-    paginator = Paginator(post_list, 10) 
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
+        'title': title,
         'page_obj': page_obj,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, template, context)
 
 
-@login_required
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.order_by('-pub_date')[:SYMBOLS_PER_PAGE]
+    post_list = group.posts.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, template, context)
+
 
 def profile(request, username):
     template = 'posts/profile.html'
@@ -38,59 +43,52 @@ def profile(request, username):
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    full_name = author.get_full_name()
     context = {
+        'author': author,
         'page_obj': page_obj,
-        'post_list': post_list,
-        'full_name': full_name,
     }
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
-    template = 'posts/post_detail.html'
+    template = "posts/post_detail.html"
     post = get_object_or_404(Post, id=post_id)
-    user = get_object_or_404(User, id=post.author.id)
-    full_name = user.get_full_name()
-    post_count = user.posts.select_related().count()
     context = {
-        'post': post,
-        'user': user,
-        'full_name': full_name,
-        'post_count': post_count,
+        "post": post,
     }
     return render(request, template, context)
 
 
+@login_required
 def post_create(request):
-    template= 'posts/create_post.html'
+    template = "posts/create_post.html"
     form = PostForm(request.POST or None)
+    context = {
+        'form': form
+    }
     if not form.is_valid():
-        return render(request, template, {'form': form})
+        return render(request, template, context)
     post = form.save(commit=False)
     post.author = request.user
     post.save()
-    return redirect(f'profile/{request.user.username}/')
+    return redirect('posts:profile', username=request.user.username)
 
 
+@login_required
 def post_edit(request, post_id):
-    template = 'posts/create_post.html'
-    post = get_object_or_404(Post, pk=post_id)
-    if post.author != request.user:
-        return redirect(
-            'posts:post_detail', post_id
-        )
-    form = PostForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=post
+    post = get_object_or_404(
+        Post,
+        id=post_id
     )
-    if form.is_valid():
-        form.save()
-        return redirect(
-            'posts:post_detail', post_id
-        )
-    return render(request, template, {
-        'form': form, 'is_edit': True, 'post': post
-    })
+    if request.user != post.author:
+        return redirect('posts:post_detail', post_id=post_id)
+    form = PostForm(request.POST, instance=post)
+    if not form.is_valid():
+        return render(request, 'posts/create_post.html', {
+            'form': form,
+            'post_id': post_id,
+            'is_edit': True
+        })
+    form.save()
+    return redirect('posts:post_detail', post_id=post_id)
 
